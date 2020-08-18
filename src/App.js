26f561a09dc74c772064d6d6121e15b731237a45
Zahar Zagrava <zaharzagrava@gmail.com>
2020-08-18 18:21:03 +0300

    Added index and admin pages

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import Admin from './components/Admin/Admin'
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <>
-      <Admin />
-{/* 
+
       <Switch>
-        <Route exact path={["/", "/library", "/wishlist", "/category", "/bestsellers", "/cart"]}>
-          <div className={styles.wrapper}>
-            <Header />
-            <LibraryBooks />
-            <Slider />
-          </div>
-          <Footer />
+        <Route exact path={["/"]}>
+          <Register />
         </Route>
-      </Switch> */}
+        <Route exact path={["/admin"]}>
+          <Admin />
+        </Route>
+      </Switch>
     </>
   );
 }
