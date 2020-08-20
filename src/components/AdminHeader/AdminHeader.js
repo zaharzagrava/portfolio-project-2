cc2f18effd8d29a4761686ddd7b103f252a519cc
Zahar Zagrava <zaharzagrava@gmail.com>
2020-08-20 10:13:18 +0300

    Deleted MenuIcon

--- src/components/AdminHeader/AdminHeader.js
+++ src/components/AdminHeader/AdminHeader.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import styles from './AdminHeader.module.scss'
 
-import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 function AdminHeader() {
   return (
     <div className={styles.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={styles.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={styles.title}>
             Restomaster
           </Typography>
